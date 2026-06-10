416e16a7498f8b2b805d252460485250b324ac44
Marcos Osorio <marcos2106@hotmail.com>
2026-06-10 15:57:56 -0300

    Correção porcetagem dos palpites

--- src/main/java/br/com/bolao/bolao10/model/ApostaPartida.java
+++ src/main/java/br/com/bolao/bolao10/model/ApostaPartida.java
@@ -30,27 +30,65 @@ public class ApostaPartida implements Serializable {
 	public void setNumSelecaoB(Integer numSelecaoB) {
 		this.numSelecaoB = numSelecaoB;
 	}
+
+	private int valorSeguro(Integer valor) {
+		return valor == null ? 0 : valor;
+	}
 	
 	public Integer getSomaTotal() {
-		return numSelecaoA + numEmpate + numSelecaoB;
+		return valorSeguro(numSelecaoA) + valorSeguro(numEmpate) + valorSeguro(numSelecaoB);
 	}
+
+	private int indiceAjuste(double[] exatos, int[] arredondados, boolean aumentar) {
+		int indice = 0;
+		double melhorScore = Double.NEGATIVE_INFINITY;
+		for (int i = 0; i < arredondados.length; i++) {
+			double score = aumentar ? (exatos[i] - arredondados[i]) : (arredondados[i] - exatos[i]);
+			if (score > melhorScore) {
+				melhorScore = score;
+				indice = i;
+			}
+		}
+		return indice;
+	}
+
+	private int[] calcularPercentuaisAjustados() {
+		int total = getSomaTotal();
+		if (total <= 0) {
+			return new int[] {0, 0, 0};
+		}
+
+		double[] exatos = new double[] {
+			(100.0 * valorSeguro(this.numSelecaoA)) / total,
+			(100.0 * valorSeguro(this.numEmpate)) / total,
+			(100.0 * valorSeguro(this.numSelecaoB)) / total
+		};
+
+		int[] arredondados = new int[] {
+			(int) Math.round(exatos[0]),
+			(int) Math.round(exatos[1]),
+			(int) Math.round(exatos[2])
+		};
+
+		int diferenca = 100 - (arredondados[0] + arredondados[1] + arredondados[2]);
+		while (diferenca != 0) {
+			boolean aumentar = diferenca > 0;
+			int indice = indiceAjuste(exatos, arredondados, aumentar);
+			arredondados[indice] += aumentar ? 1 : -1;
+			diferenca += aumentar ? -1 : 1;
+		}
+
+		return arredondados;
+	}
+
 	public Integer getPorcSelecaoA() {
-		if (getSomaTotal() > 0) {
-			return (100 * this.numSelecaoA) / getSomaTotal();
-		} 
-		return 0;
+		return calcularPercentuaisAjustados()[0];
 	}
 	public Integer getPorcEmpate() {
-		if (getSomaTotal() > 0) {
-			return (100 * this.numEmpate) / getSomaTotal();
-		} 
-		return 0;
+		return calcularPercentuaisAjustados()[1];
 	}
 	public Integer getPorcSelecaoB() {
-		if (getSomaTotal() > 0) {
-			return (100 * this.numSelecaoB) / getSomaTotal();
-		} 
-		return 0;
+		return calcularPercentuaisAjustados()[2];
 	}
 	
 }
